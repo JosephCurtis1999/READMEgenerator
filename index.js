fce2975132a33f1050d159e589af92aff78ddff5
Joe Curtis <joecurtis9999@gmail.com>
2022-02-25 09:10:21 +0000

    installed chalk modules

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const inquirer = require('inquirer');
 const chalk = require('chalk')
 inquirer.registerPrompt('recursive', require('./separatejs/generatemarkdwn.js'));
 const generateMarkdown = require('./separatejs/recursiveprompts.js')
+
+const welcome = [
+    { type: 'confirm',
+      confirm: '\b',
+      name: 'welcome',
+      message: chalk.cyanBright('Welcome to my ReadME generator. To begin please press y or enter.'),
+    },
+];
