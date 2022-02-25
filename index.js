1226adaeccfed30ca98a7e1e129b085264e4b988
Joe Curtis <joecurtis9999@gmail.com>
2022-02-25 09:51:58 +0000

    fixed generate markdown function, was incorrectly spelt in two places

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const chalk = require('chalk')
 inquirer.registerPrompt('recursive', require('./separatejs/generatemarkdwn.js'));
-const generateMarkdown = require('./separatejs/recursiveprompts.js')
+const generatemarkdwn = require('./separatejs/recursiveprompts.js')
 
 const welcome = [
     { type: 'confirm',
