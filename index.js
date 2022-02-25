c18f934212e11df67307e21b66f951c7f924117e
Joe Curtis <joecurtis9999@gmail.com>
2022-02-25 09:12:57 +0000

    added markdown tips for the user and a success message to notify them that their readme has been generated

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ const welcome = [
       message: chalk.cyanBright('Welcome to my ReadME generator. To begin please press y or enter.'),
     },
 ];
+
+const letsbegin = chalk.greenBright(`\n
+Let's Generate a README!!!
+//~~~~~~~~~~~~~~~~~~~~~~//
+     MD syntax tips
+-------------------------
+Bold    **bold text**
+Italics *italicized text*       
+Links   [title](https://www.example.com)
+Image   ![alt text](image.jpg)
+\n`);
+
+const success = chalk.greenBright(`
+Congratulations! README Generated! It's in the Output folder
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~// 
+`);
