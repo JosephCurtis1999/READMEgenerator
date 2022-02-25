3c21e0540553fae69f6613103d80004810483284
Joe Curtis <joecurtis9999@gmail.com>
2022-02-25 09:55:25 +0000

    uninstalled version 5 of chalk as there were issues, found some solutions to the problem on stack overflow, main consensus was to uninstall v5 and install version 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const fs = require('fs');
 const inquirer = require('inquirer');
-const chalk = require('chalk')
+const chalk = require('chalk');
 inquirer.registerPrompt('recursive', require('./separatejs/generatemarkdwn.js'));
 const generatemarkdwn = require('./separatejs/recursiveprompts.js')
 
